2d8560ba12e2d9c3c954c4c2083893a6751b23ed
Stephen H <Sthogue16@gmail.com>
2023-06-29 13:04:34 -0500

    moved js & css files + current time & input functs

--- Assets/script.js
+++ Assets/script.js
@@ -18,6 +18,31 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
+  // add event listener to save data for each hour
+  // add function to apply style for hour block
+  // add function to get local storage
+
+
+  var timeDisplayEl =$("#currentDay");
+   function displayTime(){
+    var currentTime = dayjs().format("dddd, MMM DD, YYYY");
+    timeDisplayEl.text(currentTime);
+   }
+   displayTime();
+
+   function saveUserInput (){
+    //gets div id for hour clicked on 
+    var hourClicked = $(this).parent().attr("id"); 
+    console.log(hourClicked);
+    // gets text value user inputs for current time
+    var textInput = $(this).parent().children("textarea").val();
+    console.log(textInput);
+    
+    // Saves user input into local storage. Name is Hour Name ID and the day so user inputs the text so
+    //they can look back to previous days if needed
+    localStorage.setItem(hourClicked + " " + dayjs().format("M/DD/YY"),JSON.stringify(textInput));
+   }
+   $("button").click(saveUserInput);
 });
